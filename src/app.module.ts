76f475441e7f0aae45b98e1daa0dd191756d4da7
Francisco Batista <francisco.batista@wizeline.com>
2020-02-29 18:02:26 -0600

    Added config from env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,50 @@
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql'
-import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { MongooseModule, MongooseModuleOptions } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EntryModule } from './entry/entry.module';
-import { AuthorModule } from './author/author.module';
-import { FirebaseModule } from './firebase/firebase.module';
-import firebaseConfig from 'src/config/firebase-config';
-import firebaseCredential from 'src/config/firebase-credential';
-import * as firebase from 'firebase-admin'
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { EntryModule } from "./entry/entry.module";
+import { AuthorModule } from "./author/author.module";
+import { FirebaseModule } from "./firebase/firebase.module";
+import firebaseConfig from "src/config/firebase-config";
+import firebaseCredential from "src/config/firebase-credential";
+import * as firebase from "firebase-admin";
+import {
+  GetMongoConfig,
+  GetGraphLQConfig,
+  GetFirebaseConfig
+} from "src/common/utils/configObjectBuildets";
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {
-      dbName: "noobsjs",
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }), 
-    GraphQLModule.forRoot({
-      debug: true,
-      playground: true,
-      autoSchemaFile: true,
-      context: ({ req }) => ({ req })
+    ConfigModule.forRoot({
+      isGlobal: true
     }),
-    FirebaseModule.forRoot({...firebaseConfig, credential: firebase.credential.cert(firebaseCredential)}),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return GetMongoConfig(configService);
+      },
+      inject: [ConfigService]
+    }),
+    GraphQLModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return GetGraphLQConfig(configService);
+      },
+      inject: [ConfigService]
+    }),
+    FirebaseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return GetFirebaseConfig(configService);
+      },
+      inject: [ConfigService]
+    }),
     EntryModule,
     AuthorModule,
     FirebaseModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
---
+++ src/common/utils/configObjectBuildets.ts
@@ -0,0 +1,63 @@
+import { ConfigService } from "@nestjs/config";
+import { MongooseModuleOptions } from "@nestjs/mongoose";
+import { GqlModuleOptions } from "@nestjs/graphql";
+import { FirebaseModuleOptions } from "../../firebase/iterfaces/FirebaseModuleOptions";
+
+import * as firebase from "firebase-admin";
+
+export const GetMongoConfig = (
+  configService: ConfigService
+): MongooseModuleOptions => {
+  return {
+    uri: configService.get<string>("MONGODB_URI"),
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
+};
+
+export const GetGraphLQConfig = (
+  configService: ConfigService
+): GqlModuleOptions => ({
+  playground: configService.get<boolean>("GRAPHQL_PLAYGROUND"),
+  debug: configService.get<boolean>("GRAPHQL_PLAYGROUND"),
+  autoSchemaFile: true,
+  context: ({ req }) => ({ req })
+});
+
+export const GetFirebaseConfig = (
+  configService: ConfigService
+): FirebaseModuleOptions => {
+  const credential = {
+    type: configService.get<string>("FIREBASE_CREDENTIAL_TYPE"),
+    project_id: configService.get<string>("FIREBASE_CREDENTIAL_PROJECT_ID"),
+    private_key_id: configService.get<string>(
+      "FIREBASE_CREDENTIAL_PRIVATE_KEY_ID"
+    ),
+    private_key: configService.get<string>("FIREBASE_CREDENTIAL_PRIVATE_KEY"),
+    client_email: configService.get<string>("FIREBASE_CREDENTIAL_CLIENT_EMAIL"),
+    client_id: configService.get<string>("FIREBASE_CREDENTIAL_CLIENT_ID"),
+    auth_uri: configService.get<string>("FIREBASE_CREDENTIAL_AUTH_URL"),
+    token_uri: configService.get<string>("FIREBASE_CREDENTIAL_TOKEN_URL"),
+    auth_provider_x509_cert_url: configService.get<string>(
+      "FIREBASE_CREDENTIAL_AUTH_PROVIDER"
+    ),
+    client_x509_cert_url: configService.get<string>(
+      "FIREBASE_CREDENTIAL_CLIENT_CERT_URL"
+    )
+  } as firebase.ServiceAccount;
+
+  console.log(credential);
+  const options = {
+    apiKey: configService.get<string>("FIREBASE_API_KEY"),
+    authDomain: configService.get<string>("FIREBASE_AUTH_DOMAIN"),
+    databaseURL: configService.get<string>("FIREBASE_DATABASE_URL"),
+    projectId: configService.get<string>("FIREBASE_PROJECT_ID"),
+    storageBucket: configService.get<string>("FIREBASE_STORAGE_BUCKET"),
+    messagingSenderId: configService.get<string>("FIREBASE_MESSAGE_SENDER_ID"),
+    appId: configService.get<string>("FIREBASE_APP_ID"),
+    measurementId: configService.get<string>("FIREBASE_MEASUREMENT_ID"),
+    credential: firebase.credential.cert(credential)
+  };
+
+  return { options };
+};
